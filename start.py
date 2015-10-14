f863afe55ee036e4d5349167f0d55c2bb4ad2244
jhonjairoroa87 <jhon.jairo@scalablepath.com>
2015-10-14 00:12:25 -0500

    added add and subtract services

--- start.py
+++ start.py
@@ -1,14 +1,34 @@
 __author__ = 'jhonjairoroa87'
 
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "is working :)"
+
+
+@app.route("/add")
+def add():
+    try:
+        first_number = int(request.args.get('a'))
+        second_number = int(request.args.get('b'))
+        return jsonify({'result': first_number + second_number})
+    except Exception as e:
+        return "There was an error" + str(e)
+
+
+@app.route("/subtract")
+def subtract():
+    try:
+        first_number = int(request.args.get('a'))
+        second_number = int(request.args.get('b'))
+        return jsonify({'result': first_number - second_number})
+    except Exception as e:
+        return "There was an error" + str(e)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
